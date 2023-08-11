456703ede339a9619a9451f7deb18ebb6af6aad9
JonAnder <jonander.ruiz@versia.com>
2023-08-11 14:02:33 +0200

    Añadir controlador

---
+++ MvcMovie/Controllers/HelloWorldController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+namespace MvcMovie.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        //
+        //GET: /HelloWorld/
+        public string Index()
+        {
+            return "This is my default action...";
+        }
+
+        //
+        //GET: /HelloWorld/Welcome/
+
+        public string Welcome(string name, int numTimes = 1)
+        {
+            return HtmlEncoder.Default.Encode($"Hola {name}, NumTimes es: {numTimes} ");
+        }
+    }
+}
