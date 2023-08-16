03854e7273a8e5b84dd67c440ce637e5cefdae65
JonAnder <jonander.ruiz@versia.com>
2023-08-16 11:03:01 +0200

    Adición de DataAnnotations en el modelo Movie

--- MvcMovie/Models/Movie.cs
+++ MvcMovie/Models/Movie.cs
@@ -1,4 +1,6 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace MvcMovie.Models
 {
@@ -6,10 +8,11 @@ namespace MvcMovie.Models
     {
         public int Id { get; set; }
         public string? Title { get; set; }
-
+        [Display(Name ="Fecha de lanzamiento")]
         [DataType(DataType.Date)]
         public DateTime RealeseDate { get; set; }
-        public string? Genre { get; set; }  
+        public string? Genre { get; set; }
+        [Column(TypeName ="decimal(18, 2)")]
         public decimal Price { get; set; }  
     }
 }
